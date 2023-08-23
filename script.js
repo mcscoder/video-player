2ead496409f3b3c28cf69593d3c5facc718f2ddf
mcscoder <maicongson69@gmail.com>
2023-08-23 20:34:23 +0700

    basic feature done

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const playPauseBtn = document.querySelector(".play-pause-btn");
+const miniPlayerBtn = document.querySelector(".mini-player-btn");
+const theaterBtn = document.querySelector(".theater-btn");
+const fullScreenBtn = document.querySelector(".full-screen-btn");
 const videoContainer = document.querySelector(".video-container");
 const video = document.querySelector("video");
 
@@ -12,8 +15,52 @@ document.addEventListener("keydown", (e) => {
       togglePlay();
       break;
     case "f":
-      video.requestFullscreen();
+      toggleFullScreenMode();
+      break;
+    case "t":
+      toggleTheaterMode();
+      break;
+    case "i":
+      toggleMiniPlayerMode();
+      break;
   }
+});
+
+// View modes
+theaterBtn.addEventListener("click", toggleTheaterMode);
+fullScreenBtn.addEventListener("click", toggleFullScreenMode);
+miniPlayerBtn.addEventListener("click", toggleMiniPlayerMode);
+
+function toggleTheaterMode() {
+  videoContainer.classList.toggle("theater");
+}
+
+function toggleFullScreenMode() {
+  if (document.fullscreenElement == null) {
+    videoContainer.requestFullscreen();
+  } else {
+    document.exitFullscreen();
+  }
+}
+
+function toggleMiniPlayerMode() {
+  if (videoContainer.classList.contains("mini-player")) {
+    document.exitPictureInPicture();
+  } else {
+    video.requestPictureInPicture();
+  }
+}
+
+document.addEventListener("fullscreenchange", () => {
+  videoContainer.classList.toggle("full-screen", document.fullscreenElement);
+});
+
+video.addEventListener("enterpictureinpicture", () => {
+  videoContainer.classList.add("mini-player");
+});
+
+video.addEventListener("leavepictureinpicture", () => {
+  videoContainer.classList.remove("mini-player");
 });
 
 function togglePlay() {
